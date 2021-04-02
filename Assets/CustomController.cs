82ee4f651394f89d432dca30cd5985025156e5a3
Matt Kersey <mattkersey@Matts-MacBook-Air.local>
2021-04-02 09:13:08 -0400

    Gogo, object manipulation, and some haptics

--- Assets/CustomController.cs
+++ Assets/CustomController.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class GogoSettings
 {
-    public const float Alpha = 100.0f;
-    public const float D = 0.5f;
+    public const float Alpha = 15.0f;
+    public const float D = 0.35f;
+    public const float MaxChange = 1.0f;
 }
 
 public class CustomController : OVRGrabber
 {
+    public Transform m_headPose;
+    private float m_prevLocation = 0;
+    private float m_currentVibration = 0.0f;
+
     new void Update()
     {
         base.Update();
-        float trigger = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, m_controller);
-        float dist = (m_player.transform.position - m_parentTransform.position).magnitude;
+        float trigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, m_controller);
+        float dist = (m_headPose.position - m_parentTransform.position).magnitude;
+        // Vector3 forward = m_headPose.rotation * Vector3.forward;
+        // Quaternion rotation = new Quaternion();
+        // rotation.SetFromToRotation(forward, m_headPose.position - m_parentTransform.position);
+        // Vector3 eulerAngles = rotation.eulerAngles;
+        // for (int i = 0; i < 3; i++)
+        //     eulerAngles[i] = eulerAngles[i] > 180 ? 360 - eulerAngles[i] : eulerAngles[i];
+        
         float location = 0.0f;
         if (trigger > 0.0f && dist > GogoSettings.D)
-        {
             location = (trigger * GogoSettings.Alpha) * Mathf.Pow(dist - GogoSettings.D, 2);
-        }
-        m_gripTransform.localPosition = new Vector3(0, 0, location);
+        // if (location - m_prevLocation > GogoSettings.MaxChange)
+        //     location = m_prevLocation + GogoSettings.MaxChange;
+        // else if (m_prevLocation - location > GogoSettings.MaxChange)
+        //     location = m_prevLocation + GogoSettings.MaxChange;
+        m_anchorOffsetPosition = new Vector3(0, 0, location);
+        m_gripTransform.localPosition = m_anchorOffsetPosition;
+        m_prevLocation = location;
     }
 
     new void OnTriggerEnter(Collider other)
@@ -30,12 +46,28 @@ public class CustomController : OVRGrabber
         {
             StartCoroutine(WarnWall());
         }
+        else if (other.CompareTag("Sound Bite"))
+        {
+            m_currentVibration = 0.25f;
+            OVRInput.SetControllerVibration(m_currentVibration, m_currentVibration, m_controller);
+        }
+    }
+
+    new void OnTriggerExit(Collider other)
+    {
+        m_currentVibration = 0.0f;
+        OVRInput.SetControllerVibration(0.0f, 0.0f, m_controller);
+        // if (m_grabCandidates.Count == 0)
+        // {
+        //     m_currentVibration = 0.0f;
+        //     OVRInput.SetControllerVibration(0.0f, 0.0f, m_controller);
+        // }
     }
 
     IEnumerator WarnWall()
     {
         OVRInput.SetControllerVibration(1.0f, 1.0f, m_controller);
-        yield return new WaitForSeconds(0.5f);
-        OVRInput.SetControllerVibration(0.0f, 0.0f, m_controller);
+        yield return new WaitForSeconds(0.25f);
+        OVRInput.SetControllerVibration(m_currentVibration, m_currentVibration, m_controller);
     }
 }
