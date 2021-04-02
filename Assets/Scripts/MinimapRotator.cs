d6ae5ddecb5fc8ca2e4d2b204bf9b7e9122bffc2
Jake2206 <CLASSICJAKEALEXANDER@GMAIL.COM>
2021-04-02 09:47:10 -0400

    Created a 2d minimap

---
+++ Assets/Scripts/MinimapRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapRotator : MonoBehaviour
+{
+    public Transform player;
+
+    void Start()
+    {     
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(90f, player.localEulerAngles.y, 0f);
+    }
+}
