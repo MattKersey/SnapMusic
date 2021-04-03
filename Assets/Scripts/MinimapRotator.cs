916d157ba2942f800c35f0456cbbc1090d21745d
Jake2206 <CLASSICJAKEALEXANDER@GMAIL.COM>
2021-04-02 23:27:16 -0400

    Rotation and scale of HUD

---
+++ Assets/Scripts/MinimapRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapRotator : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 orig;
+
+    private void Start()
+    {
+        orig = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 euler = transform.rotation.eulerAngles;
+        euler.y = player.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(euler);
+    }
+}
