e92310e5df0feab94162341999770f7485007029
Carlos Eguiluz Rosas <ceguiluzrosas@gmail.com>
2021-04-04 14:21:55 -0400

    created the cubes + placeholders

---
+++ Assets/Scripts/Song-Bites/BiteController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BiteController : MonoBehaviour
+{
+    private GameObject[] songBites;
+    private int numOfTotalBites;
+    private int numOfFoundBites = 0;
+
+    private void Start()
+    {
+        songBites = GameObject.FindGameObjectsWithTag("Bite");
+        numOfTotalBites = songBites.Length;
+        RandomizeBitIdxs();
+    }
+
+    private void RandomizeBitIdxs()
+    {
+        System.Random rand = new System.Random();
+        List<int> numberList = Enumerable.Range(1, numOfTotalBites).ToList();
+        numberList.OrderBy(item => rand.Next());
+        for(int i=0; i<numOfTotalBites; i++)
+        {
+            BiteSelf _biteSelf = songBites[i].GetComponent<BiteSelf>();
+            _biteSelf.SetBiteIdx(numberList[i]);
+        }
+    }
+
+    private bool FoundAllBites()
+    {
+        return numOfFoundBites == numOfTotalBites;
+    }
+
+    public void FoundBite()
+    {
+        numOfFoundBites += 1;
+        SayHowManyFound();
+        if (FoundAllBites())
+        {
+            PlaceBitesInCenter();
+            // Teleport the player to the center of world.
+        }
+    }
+
+    private void SayHowManyFound()
+    {
+        string text = numOfFoundBites.ToString() + " out of " + numOfTotalBites.ToString() + " found.";
+        Debug.Log(text);
+    }
+
+    private void PlaceBitesInCenter()
+    {
+
+    }
+
+    public void ValidateBiteOrder()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Song-Bites/BiteSelf.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiteSelf : MonoBehaviour
+{
+
+    private BiteController _biteController;
+    private AudioSource _audioSource;
+    private int biteIdx;
+    public int rotateSpeed;
+
+    private void Start()
+    {
+        _biteController = gameObject.GetComponentInParent<BiteController>();
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+    }
+
+    public void SetBiteIdx(int idx)
+    {
+        biteIdx = idx;
+    }
+
+    private void ScaleVolume()
+    {
+        // raise or lower the volume depending on movement
+        // change color depending on volume
+        // raise -> darker
+        // lower -> lighter
+
+        // constraints
+        // min = 0.0f, max = 1.0f
+
+        float newVolume = 0.75f; // example
+        _audioSource.volume = newVolume;
+    }
+
+    private void Reverse()
+    {
+        // left and right (color?) indicators
+        // red or left-arrow -> reverse order of current song
+        // green or right-arrow -> restore order of current song
+    }
+
+    // NOTE: If not using Triggers, then delete this function
+    // and uncheck the `isTrigger` boolean in `Bite`
+    private void OnTriggerEnter(Collider other)
+    {
+        _biteController.FoundBite();
+        // add some animation (if possible)
+    }
+
+    // NOTE: If not using rigidbodies, then delete this function
+    // and remove the `Rigidbody` component from `Bite`
+    private void OnCollisionEnter(Collision collision)
+    {
+        _biteController.FoundBite();
+        // add some animation (if possible)
+    }
+}
