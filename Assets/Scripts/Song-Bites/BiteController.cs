67d787ff429e279e42bf6c57930bd3060bc2aa0a
Carlos Eguiluz Rosas <ceguiluzrosas@gmail.com>
2021-04-04 22:32:45 -0400

    attached audio to objs and verified that reverse

--- Assets/Scripts/Song-Bites/BiteController.cs
+++ Assets/Scripts/Song-Bites/BiteController.cs
@@ -7,25 +7,36 @@ using UnityEngine;
 public class BiteController : MonoBehaviour
 {
     private GameObject[] songBites;
+    private int[] orderFound;
     private int numOfTotalBites;
     private int numOfFoundBites = 0;
+
+    private float floatHeight = 2;
+    private float minX = -7;
+    private float stepSize = 2;
+
+    public GameObject placeholderParent;
 
     private void Start()
     {
         songBites = GameObject.FindGameObjectsWithTag("Bite");
         numOfTotalBites = songBites.Length;
+        orderFound = new int[songBites.Length];
         RandomizeBitIdxs();
     }
 
     private void RandomizeBitIdxs()
     {
-        System.Random rand = new System.Random();
-        List<int> numberList = Enumerable.Range(1, numOfTotalBites).ToList();
-        numberList.OrderBy(item => rand.Next());
-        for(int i=0; i<numOfTotalBites; i++)
+        System.Random random = new System.Random();
+        List<int> numberList = Enumerable.Range(0, numOfTotalBites).ToList();
+        numberList = numberList.OrderBy(item => random.Next()).ToList<int>();
+        foreach (int i in numberList) { Debug.Log("numberList: " + i); }
+
+        for (int i=0; i<numOfTotalBites; i++)
         {
             BiteSelf _biteSelf = songBites[i].GetComponent<BiteSelf>();
             _biteSelf.SetBiteIdx(numberList[i]);
+            // PlaceBiteInStage(songBites[i]); // Testing Purposes
         }
     }
 
@@ -34,13 +45,15 @@ public class BiteController : MonoBehaviour
         return numOfFoundBites == numOfTotalBites;
     }
 
-    public void FoundBite()
+    public void FoundBite(GameObject bite, int biteIdx)
     {
+        orderFound[numOfFoundBites] = biteIdx;
+        PlaceBiteInStage(bite);
         numOfFoundBites += 1;
         SayHowManyFound();
         if (FoundAllBites())
         {
-            PlaceBitesInCenter();
+            // alternative: PlaceBitesInStage(); // will need to interate on all bites
             // Teleport the player to the center of world.
         }
     }
@@ -51,14 +64,36 @@ public class BiteController : MonoBehaviour
         Debug.Log(text);
     }
 
-    private void PlaceBitesInCenter()
+    private void PlaceBiteInStage(GameObject bite)
     {
-
+        BiteSelf _biteSelf = bite.GetComponent<BiteSelf>(); // add bite to parent obj 
+        _biteSelf.stopRotating(); // make still
+        bite.transform.parent = placeholderParent.transform; // add to placeholder group
+        bite.transform.position = placeholderParent.transform.position; // reset position
+        bite.transform.position = new Vector3( // change position afterwards to avoid conflict
+            minX + stepSize,
+            bite.transform.position.y + floatHeight,
+            bite.transform.position.z
+            );
+        minX += stepSize;
     }
 
-    public void ValidateBiteOrder()
+    public bool ValidateBiteOrder()
     {
+        if (!FoundAllBites()) { return false; }
 
+        for (int idx = 0; idx<numOfTotalBites; idx++)
+        {
+            GameObject child = placeholderParent.transform.GetChild(idx).gameObject;
+            Debug.Log(child.name);
+            BiteSelf _biteSelf = child.GetComponent<BiteSelf>();
+            Debug.Log(_biteSelf.GetBiteIdx());
+            if (idx != _biteSelf.GetBiteIdx())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- Assets/Scripts/Song-Bites/BiteSelf.cs
+++ Assets/Scripts/Song-Bites/BiteSelf.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class BiteSelf : MonoBehaviour
 {
+    public int rotateSpeed;
 
     private BiteController _biteController;
     private AudioSource _audioSource;
     private int biteIdx;
-    public int rotateSpeed;
+    private bool found = false;
+    private bool continueRotating = true;
+    private string soundPath = "file://" + Application.streamingAssetsPath + "/Test-Song-1/";
+    private string audioName;
+    private AudioClip audioClip;
+
 
     private void Start()
     {
@@ -18,12 +25,57 @@ public class BiteSelf : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+        if (continueRotating)
+        {
+            transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+        }
     }
 
     public void SetBiteIdx(int idx)
     {
         biteIdx = idx;
+        audioName = "sample-" + idx.ToString() + ".mp3";
+        StartCoroutine(LoadAudio());
+    }
+
+    public void stopRotating()
+    {
+        continueRotating = false;
+    }
+
+    public int GetBiteIdx()
+    {
+        return biteIdx;
+    }
+
+    // Source: https://youtu.be/9gAHZGArDgU
+    // Note: The type is deprecated so I tried upgrading it to the suggested type
+    // (UnityWebRequest) but there was a method issue with the 3rd line, so I just
+    // reverted back. 
+    private IEnumerator LoadAudio()
+    {
+        WWW request = GetAudioFromFile(soundPath, audioName);
+        yield return request;
+
+        audioClip = request.GetAudioClip();
+        audioClip.name = audioName;
+        _audioSource.clip = audioClip;
+        // to verify attachment, just unclick "mute" on the `AudioSouce` component
+        PlayAudioFile();
+        Reverse(); // uncomment to verify play in reverse order
+    }
+
+    private void PlayAudioFile()
+    {
+        _audioSource.Play();
+        _audioSource.loop = true;
+    }
+
+    private WWW GetAudioFromFile(string path, string filename)
+    {
+        string audioToLoad = string.Format(path + "{0}", filename);
+        WWW request = new WWW(audioToLoad);
+        return request;
     }
 
     private void ScaleVolume()
@@ -35,8 +87,7 @@ public class BiteSelf : MonoBehaviour
 
         // constraints
         // min = 0.0f, max = 1.0f
-
-        float newVolume = 0.75f; // example
+        float newVolume = 0.75f; // prefab default is 0.5
         _audioSource.volume = newVolume;
     }
 
@@ -45,21 +96,19 @@ public class BiteSelf : MonoBehaviour
         // left and right (color?) indicators
         // red or left-arrow -> reverse order of current song
         // green or right-arrow -> restore order of current song
+
+        // Source: https://forum.unity.com/threads/playing-audio-backwards.95770/
+        _audioSource.pitch = -1; // to undo the reverse, just set to 1
+        //_audioSource.timeSamples = _audioSource.clip.samples - 1;  // keep for now...
     }
 
-    // NOTE: If not using Triggers, then delete this function
-    // and uncheck the `isTrigger` boolean in `Bite`
     private void OnTriggerEnter(Collider other)
     {
-        _biteController.FoundBite();
-        // add some animation (if possible)
-    }
-
-    // NOTE: If not using rigidbodies, then delete this function
-    // and remove the `Rigidbody` component from `Bite`
-    private void OnCollisionEnter(Collision collision)
-    {
-        _biteController.FoundBite();
-        // add some animation (if possible)
+        if (!found)
+        {
+            _biteController.FoundBite(gameObject, biteIdx);
+            found = true;
+            // add some animation (if possible)
+        }
     }
 }
