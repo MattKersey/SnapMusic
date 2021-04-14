aa9980167a20c09e698cb99175616685a225f008
Matt Kersey <mkersey95@gmail.com>
2021-04-14 11:03:27 -0400

    Labels draft

---
+++ Assets/ButtonLabelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonLabelController : MonoBehaviour
+{
+    public Transform headPose;
+    public string labelText;
+    protected GameObject line;
+    protected GameObject label;
+    protected RectTransform labelTransform;
+    void Start()
+    {
+        line = transform.Find("Line").gameObject;
+        label = transform.Find("Canvas").gameObject;
+        label.transform.Find("Text").GetComponent<Text>().text = labelText;
+        labelTransform = label.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (label.activeSelf)
+        {
+            labelTransform.forward = labelTransform.position - headPose.position;
+        }
+    }
+
+    // Update is called once per frame
+    public void Activate(bool isActive)
+    {
+        line.SetActive(isActive);
+        label.SetActive(isActive);
+    }
+}
--- Assets/Scripts/AdditionalControls.cs
+++ Assets/Scripts/AdditionalControls.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class AdditionalControls : MonoBehaviour
 {
+    public ButtonLabelController[] labelControllers;
     protected OVRPlayerController playerController;
+    protected bool labelsActive = false;
 
     void Start()
     {
         playerController = GetComponent<OVRPlayerController>();
+        foreach (ButtonLabelController labelController in labelControllers)
+        {
+            labelController.Activate(labelsActive);
+        }
     }
 
     void Update()
@@ -29,6 +35,16 @@ public class AdditionalControls : MonoBehaviour
         {
             Debug.Log("l2");
             CustomController.Undo();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            Debug.Log("Start");
+            labelsActive = !labelsActive;
+            foreach (ButtonLabelController labelController in labelControllers)
+            {
+                labelController.Activate(labelsActive);
+            }
         }
     }
 }
