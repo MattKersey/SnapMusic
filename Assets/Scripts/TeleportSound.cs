38b45e7ec15466356bad46c8a162c505c94aab75
Jake2206 <CLASSICJAKEALEXANDER@GMAIL.COM>
2021-04-18 22:32:54 -0400

    Teleport sounds converted to FMOD

---
+++ Assets/Scripts/TeleportSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class TeleportSound : StudioEventEmitterOcclusion
+{
+    void Start()
+    {
+        fmodInstance = RuntimeManager.CreateInstance(fmodEvent);
+        RuntimeManager.AttachInstanceToGameObject(fmodInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        //fmodInstance.start();
+    }
+
+    public void PlaySound()
+    {
+        fmodInstance.start();
+    }
+
+    public void StopSound()
+    {
+        fmodInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
